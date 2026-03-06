cf97bf63f490ffc8018ff38c21c49405b51a1932
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-03-06 11:10:31 +0700

    Add files via upload

---
+++ js/script copy 4.js
@@ -0,0 +1,70 @@
+
+const menuLinks = document.querySelectorAll(".menu-link");
+const sections = document.querySelectorAll(".content-section");
+const title = document.getElementById("sectionTitle");
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+
+        menuLinks.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+
+        sections.forEach(sec => sec.classList.remove("active"));
+
+        const target = link.dataset.target;
+        document.getElementById(target).classList.add("active");
+
+        title.textContent = link.textContent;
+    });
+});
+
+
+// Skill animation
+const bars = document.querySelectorAll(".bar div");
+setTimeout(() => {
+    bars.forEach(bar => {
+        bar.style.width = bar.getAttribute("style").replace("width:", "");
+    });
+}, 500);
+
+// Typing animation
+const text = "Teddy Mulyana";
+let index = 0;
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(index); index++;
+        setTimeout(typeEffect, 100);
+    }
+} typeEffect(); // Counter animation let years=0; let targetYears=24; const
+counter = document.getElementById("yearCounter"); let counterInterval = setInterval(() => {
+    years++;
+    counter.textContent = years;
+    if (years >= targetYears) clearInterval(counterInterval);
+}, 80);
+
+// Skill animation
+document.querySelectorAll(".bar div").forEach(bar => {
+    setTimeout(() => {
+        bar.style.width = bar.dataset.width;
+    }, 500);
+});
+
+// Expandable
+document.querySelectorAll(".expand-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const content = btn.nextElementSibling;
+        content.style.display = content.style.display === "block" ? "none" : "block";
+    });
+});
+
+document.getElementById("themeToggle").addEventListener("click", () => {
+    document.body.classList.toggle("light");
+});
+
+// Expandable timeline
+document.querySelectorAll(".expand-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const content = btn.nextElementSibling;
+        content.style.display = content.style.display === "block" ? "none" : "block";
+    });
+});
