6172412355ebec8e0393c29f0e9dad179ee6f776
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-04-10 14:06:39 +0700

    Delete js directory

--- js/script copy 2.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".card button").forEach(btn => {
-    btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        const link = btn.closest(".card").getAttribute("href");
-        window.open(link, "_blank");
-    });
-});
--- js/script copy.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".card button").forEach(button => {
-    button.addEventListener("click", () => {
-        alert("Feature coming soon 🚀");
-    });
-});
