dbcd6477c0e5f75bc7c06856b490a270c3198236
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-03 18:19:47 +0700

    Create plugin.js

---
+++ qr/plugins/whatsapp/plugin.js
@@ -0,0 +1,25 @@
+class Plugin{
+
+    run(){
+
+        const phone="6285158822803";
+
+        const msg=`Halo pak.Teddy
+Nama saya :
+
+tolong kirim lokasi terkini Bapak ya.
+saya perlu tahu seberapa jauh dari saya.
+
+(this feature is powered by https://silverhawk.web.id)`;
+
+        const url=
+        "https://wa.me/"
+        +phone
+        +"?text="
+        +encodeURIComponent(msg);
+
+        location.href=url;
+
+    }
+
+}
