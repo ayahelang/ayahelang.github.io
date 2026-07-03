e11600aaf48c3228c99da9ab7b93da0580001d77
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-03 18:34:49 +0700

    Update admin.js

--- qr/admin/admin.js
+++ qr/admin/admin.js
@@ -51,3 +51,63 @@ break;
 }
 
 dashboard();
+
+function dashboard(){
+
+page.innerHTML=`
+
+<h2>
+
+SilverQR CMS
+
+</h2>
+
+<br>
+
+<h3>
+
+Current Mode
+
+</h3>
+
+<select id="mode">
+
+<option>AUTO</option>
+
+<option>DRIVER</option>
+
+<option>TEACHING</option>
+
+<option>PROMOTION</option>
+
+</select>
+
+<br><br>
+
+<button id="saveMode">
+
+SAVE
+
+</button>
+
+`;
+
+document
+.getElementById("saveMode")
+.onclick=()=>{
+
+Storage.save(
+
+"mode",
+
+document
+.getElementById("mode")
+.value
+
+);
+
+alert("Saved");
+
+};
+
+}
